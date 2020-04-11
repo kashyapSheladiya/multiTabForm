848aca536121c61625aecc8c206e0955c61d6793
kashyapSheladiya <kashyap27041993@gmail.com>
2020-04-11 23:48:17 +0200

    index, create, new action

--- app/controllers/admissions_controller.rb
+++ app/controllers/admissions_controller.rb
@@ -4,9 +4,20 @@ class AdmissionsController < ApplicationController
   end
 
   def create
-    render json: params
-    return
+    @admission = Admission.new(admission_params)
+    @admission.save if @admission.valid?
+    redirect_to new_admission_path
   end
-  
-  
+
+  def index
+    @admissions = Admission.all
+  end
+
+  private
+
+  def admission_params
+    params.require(:admission).permit(:name, :dob, :gender, :email, :country, :address,
+      :dadname, :momname, :dadoccupation, :momoccupation,
+      :secmarks, :highsecmarks, :media_1, :media_2, :media_3)
+  end  
 end
